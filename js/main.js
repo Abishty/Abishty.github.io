864ce57720bba2299a53701866cb49a487ac8ea6
ab-bisht <18abhishek.b@gmail.com>
2020-07-15 18:30:01 +0530

    new changes done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
 function main() {
-
 (function () {
    'use strict';
 
@@ -86,7 +85,29 @@ function main() {
     ======================================*/
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false
-	});	
+  });	
+  
+  // Clients Gallery
+  $('.customer-logos').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+      pauseOnHover: false,
+      responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 2
+      }
+    }]
+  });
 
 }());
 
